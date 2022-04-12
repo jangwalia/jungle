fff2ffdeeff71fa5f3f50375057e202dbffdc6bd
jang walia <jangiwalia83@gmail.com>
2022-04-12 13:26:57 -0700

    inspect the order details

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,9 @@
 class OrdersController < ApplicationController
 
   def show
+    
     @order = Order.find(params[:id])
+    
   end
 
   def create
@@ -43,6 +45,7 @@ class OrdersController < ApplicationController
     )
 
     enhanced_cart.each do |entry|
+    
       product = entry[:product]
       quantity = entry[:quantity]
       order.line_items.new(
